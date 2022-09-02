cdaa3765664b2344bbeb5b96ac607c77aeea44a2
UnivDonguk22 <kampo5096@hanyang.ac.kr>
2022-09-02 14:36:42 +0900

    9/2 Class time and gui

--- FIrst/src/main.java
+++ FIrst/src/main.java
@@ -5,13 +5,11 @@ import java.time.*;
 public class main {
 
 	//public static void main(String[] args) {
-	//	JOptionPane.showMessageDialog(null, "Hello World!");
-
+	//JOptionPane.showMessageDialog(null, "Hello World!");
 	//}
-	
+
 	public static void main(String[] args) {
 		System.out.println(LocalTime.now());
 	}
 
-	
 }
