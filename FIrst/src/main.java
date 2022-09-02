4763cb111049211655f2f847d40f396b83137363
UnivDonguk22 <kampo5096@hanyang.ac.kr>
2022-09-02 13:51:59 +0900

    9/2 Class

--- FIrst/src/main.java
+++ FIrst/src/main.java
@@ -1,9 +1,17 @@
 
+import javax.swing.*;
+import java.time.*;
+
 public class main {
 
-	public static void main(String[] args) {
-		System.out.println("Hello World");
+	//public static void main(String[] args) {
+	//	JOptionPane.showMessageDialog(null, "Hello World!");
 
+	//}
+	
+	public static void main(String[] args) {
+		System.out.println(LocalTime.now());
 	}
 
+	
 }
