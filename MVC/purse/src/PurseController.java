b8f0c260b45f4ceb450faac4c20ac9564be62fab
UnivDonguk22 <kampo5096@hanyang.ac.kr>
2022-10-05 23:30:19 +0900

    :sparkles: Finished Purse
    한국 지갑 & 일본 지갑 & 환전기능 추가완료

--- MVC/purse/src/PurseController.java
+++ MVC/purse/src/PurseController.java
@@ -32,7 +32,7 @@ public class PurseController {
 		message += "수입 +, 지출 -, 한화 K, 일화 J, 종료 Q";
 		
 		char request = input.readRequest(message);
-		message = "+,- 키 중 하나를 누르고 OK 단추를 누르세요.";
+		message = "+,-, > , < 키 중 하나를 누르고 OK 단추를 누르세요. (+ : 수입, - : 지출, > : 한일, < : 일한)";
 
 		int amount;
 		
@@ -40,6 +40,8 @@ public class PurseController {
 			outputKorea.showTransaction("서비스를 마칩니다.");
 			return;
 		}
+		
+		
 		
 		
 		/*
@@ -52,10 +54,37 @@ public class PurseController {
 			outputJapan.showTransaction("비활성");
 			
 			
-			// new input with '+' or '-'
+			// new input with '+' or '-' or '<' or '>'
 			request = input.readRequest(message);
 			
-			if (request == '+') {
+			if (request == '<') {
+				message = "금액을 입력하고 OK 단추를 누르세요.";
+				amount = input.readAmount(message);
+				if (purseJapan.spend(amount)) {
+					amount = amount * 10;
+					purseKorea.earn(amount);
+					outputJapan.showTransaction("환전 지출");
+					outputKorea.showTransaction("환전 수입");
+				}
+				else {
+					outputJapan.showTransaction("환전 실패");
+				}
+			}
+			else if (request == '>') {
+				message = "금액을 입력하고 OK 단추를 누르세요.";
+				amount = input.readAmount(message);
+				if (purseKorea.spend(amount)) {
+					amount = amount / 10;
+					purseJapan.earn(amount);
+					outputJapan.showTransaction("환전 지출");
+					outputKorea.showTransaction("환전 수입");
+				}
+				else {
+					outputJapan.showTransaction("환전 실패");
+				}
+			}
+			
+			else if (request == '+') {
 				message = "금액을 입력하고 OK 단추를 누르세요.";
 				amount = input.readAmount(message);
 				if (purseKorea.earn(amount))
@@ -79,16 +108,42 @@ public class PurseController {
 		}
 		
 		
+		
 		// If input 'J || j'
 		else if (request == 'J' || request == 'j') {
 			outputKorea.showTransaction("비활성");
 			outputJapan.showTransaction("활성");
 			
-			// new input with '+' or '-'
+			// new input with '+' or '-' or '<' or '>'
 			request = input.readRequest(message);
+			if (request == '<') {
+				message = "금액을 입력하고 OK 단추를 누르세요.";
+				amount = input.readAmount(message);
+				if (purseJapan.spend(amount)) {
+					amount = amount * 10;
+					purseKorea.earn(amount);
+					outputJapan.showTransaction("환전 지출");
+					outputKorea.showTransaction("환전 수입");
+				}
+				else {
+					outputJapan.showTransaction("환전 실패");
+				}
+			}
+			else if (request == '>') {
+				message = "금액을 입력하고 OK 단추를 누르세요.";
+				amount = input.readAmount(message);
+				if (purseKorea.spend(amount)) {
+					amount = amount / 10;
+					purseJapan.earn(amount);
+					outputJapan.showTransaction("환전 지출");
+					outputKorea.showTransaction("환전 수입");
+				}
+				else {
+					outputJapan.showTransaction("환전 실패");
+				}
+			}
 			
-			
-			if (request == '+') {
+			else if (request == '+') {
 				message = "금액을 입력하고 OK 단추를 누르세요.";
 				amount = input.readAmount(message);
 				if (purseJapan.earn(amount))
